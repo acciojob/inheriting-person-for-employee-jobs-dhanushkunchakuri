5c7dd628ec13d5c7b947ca819cb48b1ce8abf864
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-06-21 16:55:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+	greet(){
+		return `Hello, my name is ${name}, I am ${age} years old.`;
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) extends Person{
+	constructor(name,age,jobTitle){
+		this.name=name;
+		this.age=age;
+		this.jobTitle=jobTitle;
+	}
+	jobGreet(){
+		return `Hello, my name is ${name}, I am ${age} years old, and my job title is ${jobTitle}.`
+	}
+}
+
 
 // Do not change code below this line
 window.Person = Person;
